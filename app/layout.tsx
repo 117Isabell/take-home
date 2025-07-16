8f96696381c01c78ff29c525eb353cc7e759f72e
117Isabell <isabellhepburn@outlook.com>
2025-07-16 13:14:56 +0200

    feat: add compmay brand theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,34 @@
-import './globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Manrope } from 'next/font/google';
-import { getUser, getTeamForUser } from '@/lib/db/queries';
-import { SWRConfig } from 'swr';
+import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import { getUser, getTeamForUser } from "@/lib/db/queries";
+import { SWRConfig } from "swr";
 
 export const metadata: Metadata = {
-  title: 'Next.js SaaS Starter',
-  description: 'Get started quickly with Next.js, Postgres, and Stripe.'
+  title: "Sustianly - Uhlíková stopa vašej spoločnosti",
+  description:
+    "Spočítame za Vás uhlíkovú stopu Vašeho podniku pomocou informácií, s ktorými už dnes pracujete.",
 };
 
 export const viewport: Viewport = {
-  maximumScale: 1
+  maximumScale: 1,
 };
 
-const manrope = Manrope({ subsets: ['latin'] });
-
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
-    >
-      <body className="min-h-[100dvh] bg-gray-50">
+    <html lang="sk" className="bg-background text-foreground">
+      <body className="min-h-[100dvh] bg-background">
         <SWRConfig
           value={{
             fallback: {
               // We do NOT await here
               // Only components that read this data will suspend
-              '/api/user': getUser(),
-              '/api/team': getTeamForUser()
-            }
+              "/api/user": getUser(),
+              "/api/team": getTeamForUser(),
+            },
           }}
         >
           {children}
