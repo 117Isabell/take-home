631b8022710c83ad8e37c8fc1f9b7a78de8979a6
117Isabell <isabellhepburn@outlook.com>
2025-07-17 11:13:19 +0200

    feat: import footer section

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { SolutionsSection } from "@/components/sections/solutions-section";
 import { CallToAction } from "@/components/sections/call-to-action";
+import { Footer } from "@/components/sections/footer";
 
 export default function HomePage() {
   return (
@@ -25,8 +26,8 @@ export default function HomePage() {
       </section>
 
       <SolutionsSection />
-
       <CallToAction />
+      <Footer />
     </main>
   );
 }
